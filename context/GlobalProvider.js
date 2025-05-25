f6d36b38d23de19c11320ada4d63fe39434ac6f4
Rajankumar001 <chaudharyrajan387@gmail.com>
2025-05-25 11:53:33 +0530

    global provider added

--- context/GlobalProvider.js
+++ context/GlobalProvider.js
@@ -10,12 +10,8 @@ const GlobalProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [appointments, setAppointments] = useState([]);
-<<<<<<< HEAD
   const [recentAppointment, setRecentAppointment] = useState(null); 
-=======
   const [recommendation, setRecommendation] = useState(null);
->>>>>>> 5c59d3cc3612d3b1bfa828b815538349c80884ce
-
   useEffect(() => {
     const loadUser = async () => {
       try {
@@ -44,16 +40,12 @@ const GlobalProvider = ({ children }) => {
         isLoading,
         appointments,
         setAppointments,
-<<<<<<< HEAD
         recentAppointment,
         setRecentAppointment, 
-      }}
-    >
-=======
         recommendation,
         setRecommendation
-     }}>
->>>>>>> 5c59d3cc3612d3b1bfa828b815538349c80884ce
+      }}
+    >
       {children}
     </GlobalContext.Provider>
   );
